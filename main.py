bf5f3cb14da1920edfb8d79815a38d63d6eb0017
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2024-10-29 12:34:26 +0200

    asdawdqr

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-
+import sqlite3
+from flask import Flask, render_template, request
+from dati import pievienot_skolenu
 
 app = Flask(__name__)
+conn = sqlite3.connect("dati.db")
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
+    if request.method == "POST":
+        vards = request.form['name']
+        uzvards = request.form['lastname']
+        pievienot_skolenu(vards, uzvards)
+        dati = vards+" "+uzvards
+        return render_template("index.html", aizsutitais = dati)
     return render_template("index.html")
+
+@app.route("/pievienot")
+def pievienot():
+    return render_template("pievienot.html")
+
+@app.route("/atzimes")
+def atzimes():
+    return render_template("atzimes.html")
 
 if __name__ == "__main__":
     app.run(port=5000)
