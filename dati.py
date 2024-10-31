eeb6b93739bf5c7672f8b6d2472690c492924f8e
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2024-10-31 17:01:58 +0200

    viss rādās

--- dati.py
+++ dati.py
@@ -17,9 +17,40 @@ def tabulas_izveide():
 
     )
     conn.commit()
-# tabulas_izveide()
+def skolotaju_tabulas_izveide():
+    cur=conn.cursor()
+    # cur.execute("DROP TABLE cilveki")
+    cur.execute(
 
+        """
+        CREATE TABLE skolotaji(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        vards TEXT NOT NULL,
+        uzvards TEXT NOT NULL
+        )
+        """
+
+    )
+
+def prieksmetu_tabulas_izveide():
+    cur=conn.cursor()
+    # cur.execute("DROP TABLE cilveki")
+    cur.execute(
+
+        """
+        CREATE TABLE prieksmeti(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        prieksmets TEXT NOT NULL
+        )
+        """
+
+    )
+
+# tabulas_izveide()
+# skolotaju_tabulas_izveide()
+#prieksmetu_tabulas_izveide()
 def pievienot_skolenu(vards, uzvards):
+    print(vards, uzvards)
     cur=conn.cursor()
     cur.execute(
 
@@ -29,3 +60,53 @@ def pievienot_skolenu(vards, uzvards):
 
     )
     conn.commit()
+
+
+
+def pievienot_skolotaju(vards, uzvards):
+    cur=conn.cursor()
+    cur.execute(  
+        f"""
+        INSERT INTO skolotaji(vards, uzvards) VALUES("{vards}","{uzvards}")
+        """
+
+    )
+
+def pievienot_prieksmetu(prieksmets):
+    cur=conn.cursor()
+    cur.execute(  
+        f"""
+        INSERT INTO prieksmeti(prieksmets) VALUES("{prieksmets}")
+        """
+
+    )
+
+def iegut_skolenus():
+    cur=conn.cursor()
+    cur.execute(
+    
+        f"""
+        SELECT vards, uzvards FROM skoleni
+        """
+
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_skolotajus():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards, id FROM skolotaji"""
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+def iegut_prieksmetu():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT prieksmets FROM prieksmeti """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import sqlite3
 from flask import Flask, render_template, request
-from dati import pievienot_skolenu
+from dati import pievienot_skolenu, pievienot_prieksmetu, pievienot_skolotaju, iegut_skolenus, iegut_skolotajus, iegut_prieksmetu
 
 app = Flask(__name__)
 conn = sqlite3.connect("dati.db")
 
 @app.route("/", methods=["POST","GET"])
 def index():
+    skoleni_no_db = iegut_skolenus()
+    print(skoleni_no_db)
+    skolotaji_no_db = iegut_skolotajus()
+    prieksmeti_no_db = iegut_prieksmetu()
     if request.method == "POST":
         vards = request.form['name']
         uzvards = request.form['lastname']
-        pievienot_skolenu(vards, uzvards)
-        dati = vards+" "+uzvards
-        return render_template("index.html", aizsutitais = dati)
-    return render_template("index.html")
+        skolotajs_v = request.form['sk_name']
+        skolotajs_uzv = request.form['sk_lastname']
+        prieksmets = request.form['prieksmets']
+        if vards and uzvards:
+            pievienot_skolenu(vards, uzvards)
+        if skolotajs_uzv and skolotajs_v:
+            pievienot_skolotaju(skolotajs_v, skolotajs_uzv)
+        if prieksmets:
+            pievienot_prieksmetu(prieksmets)
+        dati = f"pievienotais skolēns - {vards} {uzvards}, skolotājs - {skolotajs_v} {skolotajs_uzv}, priekšmets {prieksmets}"
+        return render_template("index.html", aizsutitais = dati, skoleni = skoleni_no_db, skolotaji = skolotaji_no_db, prieksmets = prieksmeti_no_db)
+    #get metode
+    return render_template("index.html", skoleni = skoleni_no_db, skolotaji = skolotaji_no_db, prieksmets = prieksmeti_no_db)
 
 @app.route("/pievienot")
 def pievienot():
-    return render_template("pievienot.html")
+    skolotaji = iegut_skolotajus()
+    if request.method == "POST":
+        print(request.form['skolotajs'])
+    return render_template("pievienot.html", skolotaji = skolotaji)
 
 @app.route("/atzimes")
 def atzimes():
